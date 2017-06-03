fc0097b3c697b58b4cf892bec7b2ef8a7107572f
Michael Dunnegan <mdunnegan@spicy.io>
2017-06-03 17:46:26 -0400

    Updated welcome page to display different text when you're signed in/out

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default () => {
-	return (
-		<div>Welcome to our slice of paridise!</div>
-	);
+class Welcome extends Component {
+  render() {
+    if (this.props.authenticated) {
+      return <div>Thanks for being a part of this experiment!</div>
+    } else {
+      return <div>Welcome to Drum Hub! Log in or create an account to start rocking out</div>
+    }
+  }
 }
+
+function mapStateToProps(state) {
+  return {
+    authenticated: state.auth.authenticated
+  }
+}
+
+export default connect(mapStateToProps)(Welcome);
